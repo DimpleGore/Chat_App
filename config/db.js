f4f5abb58a0e63eb2c0a8d332cffbe39ed6e84b7
DimpleGore <80614815+DimpleGore@users.noreply.github.com>
2023-01-03 01:29:20 +0530

    fourth commit

--- config/db.js
+++ config/db.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async() => {
     try{
+        mongoose.set('strictQuery',false)
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
         })
+       
         console.log(`MongDB Connected: ${conn.connection.host}`);
 
     }catch(error){
